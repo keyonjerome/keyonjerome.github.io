36d581d94bca9006b3b86b2ab0a4f0e7f6ab1350
Keyon-Jerome <keyonjerome@gmail.com>
2020-03-21 08:05:40 -0400

    Added skeleton portfolio intro page

--- keyonjerome-github-io/src/app/app.module.ts
+++ keyonjerome-github-io/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,6 +11,7 @@ import { PortfolioPageComponent } from './portfolio-page/portfolio-page.componen
 import { PortfolioIntroComponent } from './portfolio-intro/portfolio-intro.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {ScrollingModule} from '@angular/cdk/scrolling';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -22,9 +24,10 @@ import {ScrollingModule} from '@angular/cdk/scrolling';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
     NgbModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
