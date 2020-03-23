e45f93dbfc3076d04dec80c104ab9a821749c4d6
Keyon-Jerome <keyonjerome@gmail.com>
2020-03-23 13:06:20 -0400

    Working, but unfinished, RBC page.

--- keyonjerome-github-io/src/app/rbcapplication/rbcapplication.component.ts
+++ keyonjerome-github-io/src/app/rbcapplication/rbcapplication.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {} from '@fortawesome/free-regular-svg-icons';
+import {faGithub,faFacebook,faLinkedinIn,} from '@fortawesome/free-brands-svg-icons';
+import {faAt,faMailBulk,faHeart} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-rbcapplication',
   templateUrl: './rbcapplication.component.html',
   styleUrls: ['./rbcapplication.component.css']
 })
 export class RBCApplicationComponent implements OnInit {
-
+  faHeart =faHeart;
+  faGithub = faGithub;
+  faFacebook = faFacebook;
+  faLinkedinIn = faLinkedinIn;
+  faAt = faAt;
   constructor() { }
 
   ngOnInit(): void {
